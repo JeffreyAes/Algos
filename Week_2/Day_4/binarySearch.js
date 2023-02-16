f6967a5be29f1442a4177424864cd9ca240dbf96
Jeffrey <aeschlimanjeffrey@gmail.com>
2023-02-16 08:46:23 -0800

    clean up

--- Week_2/Day_4/binarySearch.js
+++ Week_2/Day_4/binarySearch.js
@@ -27,9 +27,6 @@ var search = function (nums, target) {
     let start = 0;
     let end = nums.length - 1
 
-    // if (nums[mid] === target) {
-    //     return mid
-    // }
     while (start <= end) {
         let mid = Math.floor((start + end) / 2)
         if (nums[mid] < target) {
