54bcaaec986b541d65f887472e324aff48b32bda
Jeffrey <aeschlimanjeffrey@gmail.com>
2023-02-21 11:56:23 -0800

    threeSUm

--- Week_3/Day_1/threeSum.js
+++ Week_3/Day_1/threeSum.js
@@ -14,22 +14,50 @@ Notice that the solution set must not contain duplicate triplets.
 const nums1 = [-1, 0, 1, 2, -1, -4]
 // Output: [[-1,-1,2],[-1,0,1]]
 // Explanation:
-    // nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
-    // nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
-    // nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
-    // The distinct triplets are [-1,0,1] and [-1,-1,2].
-    // Notice that the order of the output and the order of the triplets does not matter.
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Notice that the order of the output and the order of the triplets does not matter.
 
-const nums2 = [0,1,1]
+const nums2 = [0, 1, 1]
 // Output: []
 // Explanation: The only possible triplet does not sum up to 0.
 
-const nums3 = [0,0,0]
+const nums3 = [0, 0, 0]
 // Output: [[0,0,0]]
 // Explanation: The only possible triplet sums up to 0.
 
-var threeSum = function(nums) {
-        
+var threeSum = function (nums) {
+    const res = [];
+    nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length; i++) {
+        const a = nums[i];
+        if (a > 0) break
+
+        if (i > 0 && a === nums[i - 1]) continue;
+        let l = i + 1;
+        let r = nums.length - 1;
+        while (l < r) {
+            const threeSum = a + nums[l] + nums[r];
+            if (threeSum > 0) {
+                r--
+            }
+            else if (threeSum < 0) {
+                l++
+            }
+            else {
+                res.push([a, nums[l], nums[r]]);
+                l++
+                r--
+                while (nums[l] === nums[l - 1]) {
+                    l++
+                }
+            }
+        }
+    }
+    return res
+
 };
 
 console.log(threeSum(nums1))
